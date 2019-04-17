949dfc88878dd6f70af5e037f7eff62c573b9aff
Peng Xiao <xiaoquwl@gmail.com>
2019-04-17 23:03:43 +0200

    blog list view
    Signed-off-by: Peng Xiao <xiaoquwl@gmail.com>

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'blog'
+urlpatterns = [
+    # post views
+    path('', views.post_list, name='post_list'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(
+        request,
+        'blog/post/list.html',
+        {'posts': posts}
+    )
