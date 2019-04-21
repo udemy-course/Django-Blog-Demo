2f6203442219937469d36619eb894c605f510e7c
Peng Xiao <xiaoquwl@gmail.com>
2019-04-21 11:10:09 +0200

    blog list link to detail
    Signed-off-by: Peng Xiao <xiaoquwl@gmail.com>

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -30,3 +31,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_url(self):
+        return reverse(
+            'blog:post_detail',
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug
+            ]
+        )
